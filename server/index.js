d3d61c5cfeb08242197d03a086f0e720045114c9
Andrew McDonald <andrewmcd.dev@gmail.com>
2022-03-31 14:15:41 -0400

    Used envrionment variables

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const usersController = require("./controllers/users");
 const postsController = require("./controllers/posts");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use("/", express.static(__dirname + "/public/"))
     .use(express.json())
     .get("/api", (req, res) => {
