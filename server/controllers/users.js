051b03920a4ca7eed8b6c8293ceae861fe0ab42c
AndrewMcDonald-Dev <AndrewMcDonald.Dev@gmail.com>
2022-05-02 02:41:40 +0000

    Deploying to node-deploy from @ AndrewMcDonald-Dev/AdvWebProj@20416df2cfb92fa20679e3094c9edfede11dd357 🚀

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,26 +1,117 @@
-const express = require("express");
+const express = require('express');
 const app = express.Router();
-const userModel = require("../models/user");
-const StatusCodes = require("http-status-codes");
 
-app.get("/", (req, res) => {
-    res.send(userModel.list);
+const { requireAuth } = require('../models/auth');
+const userModel = require('../models/user');
+const { StatusCodes } = require('http-status-codes');
+
+app.get('/', requireAuth, (req, res, next) => {
+    userModel
+        .getList()
+        .then((users) => {
+            res.status(StatusCodes.OK).send({
+                success: true,
+                errors: [],
+                data: users,
+            });
+        })
+        .catch(next);
 })
-    .get("/:id", (req, res) => {
-        const user = userModel.get(req.params.id);
-        res.send(user);
+    .get('/handle/:handle', (req, res, next) => {
+        userModel
+            .getByHandle(req.user.handle)
+            .then((user) => {
+                res.status(StatusCodes.OK).send({
+                    success: true,
+                    errors: [],
+                    data: user,
+                });
+            })
+            .catch(next);
     })
-    .post("/", (req, res) => {
-        const user = userModel.create(req.body);
-        res.status(StatusCodes.CREATED).send(user);
+    .get('/:id', (req, res, next) => {
+        userModel
+            .get(req.params.id)
+            .then((user) => {
+                res.status(StatusCodes.OK).send({
+                    success: true,
+                    errors: [],
+                    data: user,
+                });
+            })
+            .catch(next);
     })
-    .delete("/:id", (req, res) => {
-        const user = userModel.remove(req.params.id);
-        res.send({ success: true, errors: [], data: user });
+    .post('/', (req, res, next) => {
+        userModel
+            .create(req.body)
+            .then((user) => {
+                res.status(StatusCodes.CREATED).send({
+                    success: true,
+                    errors: [],
+                    data: user,
+                });
+            })
+            .catch(next);
     })
-    .patch("/:id", (req, res) => {
-        const user = userModel.update(req.params.id, req.body);
-        res.send({ success: true, errors: [], data: user });
+    .delete('/:id', requireAuth, (req, res, next) => {
+        userModel
+            .remove(req.params.id)
+            .then((user) => {
+                res.status(StatusCodes.OK).send({
+                    success: true,
+                    errors: [],
+                    data: user,
+                });
+            })
+            .catch(next);
+    })
+    .patch('/:id', (req, res, next) => {
+        userModel
+            .update(req.params.id, req.body)
+            .then((user) => {
+                res.status(StatusCodes.OK).send({
+                    success: true,
+                    errors: [],
+                    data: user,
+                });
+            })
+            .catch(next);
+    })
+    .post('/login', (req, res, next) => {
+        userModel
+            .login(req.body.email, req.body.password)
+            .then((user) => {
+                res.status(StatusCodes.OK).send({
+                    success: true,
+                    errors: [],
+                    data: user,
+                });
+            })
+            .catch(next);
+    })
+    .get(`/login/:token`, (req, res, next) => {
+        userModel
+            .getByToken(req.params.token)
+            .then((user) => {
+                res.status(StatusCodes.OK).send({
+                    success: true,
+                    errors: [],
+                    data: user,
+                });
+            })
+            .catch(next);
+    })
+    .post('/seed', (req, res, next) => {
+        userModel
+            .seed()
+            .then((user) => {
+                res.status(StatusCodes.OK).send({
+                    succes: true,
+                    erros: [],
+                    data: user.insertedIds,
+                });
+            })
+            .catch(next);
     });
 
 module.exports = app;
--- server/index.js
+++ server/index.js
@@ -1,16 +1,64 @@
-const express = require("express");
-const usersController = require("./controllers/users");
-const postsController = require("./controllers/posts");
+require('dotenv').config();
+const express = require('express');
+const { StatusCodes } = require('http-status-codes');
+const usersController = require('./controllers/users');
+const tasksController = require('./controllers/tasks');
+const { requireAuth } = require('./models/auth');
+const userModel = require('./models/user');
 
 const app = express();
-const port = process.env.PORT || 3000;
-app.use("/", express.static(__dirname + "/public/"))
-    .use(express.json())
-    .get("/api", (req, res) => {
-        res.send("Hello world!");
+const port = process.env.PORT || 3004;
+app.use('/', express.static(__dirname + '/public/'))
+
+    .use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header(
+            'Access-Control-Allow-Headers',
+            'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+        );
+        next();
     })
-    .use("/api/users", usersController)
-    .use("/api/posts", postsController);
+
+    .use(express.json())
+    .use((req, res, next) => {
+        const auth = req.headers.authorization;
+        if (auth) {
+            const token = auth.split(' ')[1];
+            userModel
+                .fromToken(token)
+                .then((user) => {
+                    req.user = user;
+                    next();
+                })
+                .catch(next);
+        } else next();
+    })
+    .get('/api', (req, res) => {
+        res.send('Hello world!');
+    })
+    .use('/api/users', usersController)
+    .use('/api/tasks/', /* requireAuth ,*/ tasksController)
+    .use((err, req, res, next) => {
+        console.error(err);
+        switch (err.code) {
+            case 11000:
+                res.status(StatusCodes.CONFLICT).send({
+                    success: false,
+                    errors: [
+                        {
+                            message: 'Handle already in use',
+                        },
+                    ],
+                });
+                break;
+            default:
+                res.status(
+                    err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR
+                ).send({
+                    error: err.message ?? 'Internal Server Error',
+                });
+        }
+    });
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
